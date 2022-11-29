c33f16797fc39397f31f7037f073acb87026674c
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-29 15:11:05 -0500

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,39 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
-    public static void main(String[] args){
-
+    public int sumOfNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum+=i;
+        }
+        return sum;
     }
-    
-    int sumOfNumbers(int n) {
-        return 0;
-    }
-    
+
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
+    public int gaussianSumOfNumbers(int n) {
         return 0;
     }
-   
+
     // for Extra extra credit - compare the methods and show which one is faster
-    
+
+
+    public static void main(String[] args) {
+        Scanner scanner = null;
+        Integer userInput;
+        boolean exit = false;
+        while (!exit) {
+            int sum = 0;
+            System.out.println("Please enter a number" + "\n enter 0 to exit");
+            scanner = new Scanner(System.in);
+            userInput = Integer.valueOf(scanner.nextLine());
+            if(userInput==0) exit = true;
+            for (int i = 1; i <= userInput; i++) {
+                sum += i;
+            }
+            System.out.println("Sum of n:" + userInput + " is:" + sum);
+        }
+    }
 }
